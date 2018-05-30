64312439bca719253a98cecf5d50ae8ed3e6b9fd
joaopalotti <joaopalotti@gmail.com>
2018-05-30 12:27:55 +0300

    Fixed dataframe

---
+++ fix_dataframe.py
@@ -0,0 +1,32 @@
+import json
+import pandas as pd
+import ast
+import sys
+
+if len(sys.argv) <= 2:
+    print("fix_dataframe.py <ERROR> <FIX>")
+    sys.exit(0)
+
+file_with_error = sys.argv[1]
+file_with_fix = sys.argv[2]
+
+df_error = pd.read_csv(file_with_error)
+df_fix = pd.read_csv(file_with_fix)
+
+df_fix["targeting"] = df_fix["targeting"].apply(lambda x : ast.literal_eval(x))
+df_error["targeting"] = df_error["targeting"].apply(lambda x : ast.literal_eval(x))
+
+df_fix["tupled"] = df_fix["targeting"].apply(lambda x: json.dumps(x, sort_keys=True))
+df_error["tupled"] = df_error["targeting"].apply(lambda x: json.dumps(x, sort_keys=True))
+
+df_error = pd.merge(df_error, df_fix[["tupled","dau_audience","mau_audience"]], how="left", on="tupled", suffixes=("","_fixed"))
+
+df_error["mau_audience"] = df_error["mau_audience"].where(df_error["mau_audience_fixed"].isnull(), df_error["mau_audience_fixed"])
+df_error["dau_audience"] = df_error["dau_audience"].where(df_error["dau_audience_fixed"].isnull(), df_error["dau_audience_fixed"])
+
+del df_error["mau_audience_fixed"]
+del df_error["dau_audience_fixed"]
+del df_error["tupled"]
+
+df_error.to_csv(file_with_error + ".fixed")
+
