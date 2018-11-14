e0088892f39cd1ad9f2fdaf685381590eb216ab5
Joao Palotti <QCRI\jpalotti@scdev6.qcri.org>
2018-11-14 22:54:22 +0300

    Conflict with stash

--- collector.py
+++
@@ -1,9 +0,0 @@
-from pysocialwatcher import watcherAPI
-import json
-import pandas as pd
-
-watcher = watcherAPI()
-watcher.load_credentials_file("credentials.csv")
-df = watcher.run_data_collection("./venezuelans_in_brazil.json")
-
-
